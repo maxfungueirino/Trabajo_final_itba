edca954fcdd8538fe228cbd38c913e03a3383d38
maxfungueirino <maximiliano.fungue@gmail.com>
2022-10-14 15:11:48 -0300

    modificaciones finales

--- funciones_stocks.py
+++ funciones_stocks.py
@@ -53,23 +53,7 @@ def api_request (ticker, fecha_desde, fecha_hasta):
         data = response.content
         return(data)
     except:
-        print(Exception)
-        
-# Funcion para request a la api de stocks, parametros para tiempo real
-def api_request_plot (ticker, fecha_desde, fecha_hasta):
-    url = f'https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/minute/{fecha_desde}/{fecha_hasta}?adjusted=true&sort=desc&limit=5000&apiKey=6sXhLR8h0V1oqNzAgef37YroK7AJKmYE'
-
-    access_token = '6sXhLR8h0V1oqNzAgef37YroK7AJKmYE'
-    headers_auth= {"Authorization" : "Bearer {token}".format(token=access_token)}
-    
-    try:
-        response = requests.get(url, headers=headers_auth)
-
-        print(response.status_code)
-        data = response.content
-        return(data)
-    except:
-        print("Error de conexion, vuelva a intentar.")    
+        print(Exception)   
 
 # Funcion que prepara los datos para cargar en la base de datos
 def data_wrangling (df_results, ticker):
@@ -87,24 +71,6 @@ def data_wrangling (df_results, ticker):
     df_results["Dates"] = pd.to_datetime(df_results['timestamp']).dt.date
     df_results['Time'] = pd.to_datetime(df_results['timestamp']).dt.time
     df_results['Ticker'] = ticker
-    return(df_results)
-
-def data_wrangling_plot (df_results, ticker):
-    df_results.rename(columns={'v':'Volume',
-                            'vw':'volume_weighted',
-                            'o':'Open',
-                            'c':'Close',
-                            'h':'High',
-                            'l':'Low',
-                            't':'timestamp',
-                            'n':'n_transactions'},
-                             inplace=True)
-
-    df_results['timestamp']=pd.to_datetime(df_results['timestamp'], unit='ms')
-    df_results["Dates"] = pd.to_datetime(df_results['timestamp'])
-    df_results['Time'] = pd.to_datetime(df_results['timestamp'])
-    df_results['Ticker'] = ticker
-    df_results.sort_values(by=['Dates'], inplace=True, ascending=False)
     return(df_results)
 
 def graficar (df_plot, ticker):
